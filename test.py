571d99c9dd87bac29926f2dc483cec25f1e70141
Ulises <ulisejg@outlook.com>
2025-06-23 22:45:51 -0600

    Created utils with function to get mutual info

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 from BayesNet import BayesNet
+from utils import get_mutual_info
 import numpy as np
 import pandas as pd
 
-test_net = BayesNet('test')
-test_net.add_node('A')
-test_net.add_node('B')
-test_net.add_node('D')
+# test_net = BayesNet('test')
+# test_net.add_node('A')
+# test_net.add_node('B')
+# test_net.add_node('D')
 
-test_net.add_edge('A', 'B')
-test_net.add_edge('B', 'C')
+# test_net.add_edge(('A', 'B'))
+# test_net.add_edge(('B', 'C'))
 
-print(test_net.get_nodes(), test_net.graph['Edges'], test_net.get_orphans())
-print(test_net.top_sort())
+# print(test_net.get_nodes(), test_net.graph['Edges'], test_net.get_roots())
+# test_net.reverse_edge(('A', 'B'))
+# print(test_net.get_edges())
+
+data = {
+    'x1': [1,1,0,1,0,0,1,0,1,0],
+    'x2': [0,1,0,1,0,1,1,0,1,0],
+    'x3': [0,1,1,1,0,1,1,0,1,0]
+    }
+
+df = pd.DataFrame(data)
+
+
+
+print(get_mutual_info(df, 'x1', 'x2'))
---
+++ utils.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+def get_mutual_info(df: pd.DataFrame, var1:str, var2:str) -> float:
+    """
+    Function to calculate mutual information between two variables in a 
+    database.
+    Inputs: database as a pandas Dataframe, variable names as string.
+    Returns: Mutual information as a float
+    """
+    
+    # Calculate joint probabilities of both variables, given data observer
+    # in dataframe
+    freq_table = df.groupby([var1, var2]).size().reset_index(name='count')
+    freq_table['Pxy'] = freq_table['count']/len(df)
+    joint_probs = freq_table.drop('count', axis=1)
+    
+    # Get marginal probabilities
+    Px = df[var1].value_counts(normalize=True).to_dict()
+    Py = df[var2].value_counts(normalize=True).to_dict()
+    
+    mutual_info = .0
+    
+    for _, row in joint_probs.iterrows():
+        x = row[var1]
+        y = row[var2]
+        
+        pxy = row['Pxy']
+        px = Px[x]
+        py = Py[y]
+        
+        mutual_info += pxy * np.log2(pxy / (px*py))
+        
+    return mutual_info
