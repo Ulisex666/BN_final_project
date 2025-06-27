df7c785a44e585041182d5dd23d27b5b39dcfc94
Ulises <ulisejg@outlook.com>
2025-06-26 23:17:46 -0600

    Created function to export BayesNet to pyAgrum

--- BayesNet/utils.py
+++ BayesNet/utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import pyagrum as gum
+from BayesNet.BayesNet import BayesNet
 from sklearn.preprocessing import KBinsDiscretizer
 from pandas.plotting import table
 
@@ -35,5 +37,44 @@ def get_mutual_info(df: pd.DataFrame, var1:str, var2:str) -> float:
         
     return mutual_info
 
+def export_to_pyagrum(bn: BayesNet) -> gum.BayesNet:
+    """
+    Convierte un objeto BayesNet propio a un objeto pyAgrum.BayesNet.
+    """
+    gum_bn = gum.BayesNet(bn.BN_name)
+
+    # 1. Agregar variables
+    var_mapping = {}  # Mapea nombre de variable a ID de pyAgrum
+    for var_name in bn.get_nodes():
+        var_values = bn.graph['Nodes'][var_name].get_var_values()
+        pyagrum_var = gum.LabelizedVariable(var_name, var_name, len(var_values))
+        for val in var_values:
+            pyagrum_var.changeLabel(var_values.index(val), str(val))
+        var_id = gum_bn.add(pyagrum_var)
+        var_mapping[var_name] = var_id
+
+    # 2. Agregar arcos
+    for parent, child in bn.get_edges():
+        gum_bn.addArc(var_mapping[parent], var_mapping[child])
+
+    # 3. Agregar CPTs
+    for var_name in bn.get_nodes():
+        cpt = bn.get_CPT(var_name).to_dataframe()
+        var_vals = bn.graph['Nodes'][var_name].get_var_values()
+        parents = bn.get_parents(var_name)
+        
+        # Si no tiene padres (nodo raíz)
+        if not parents:
+            prob_vector = [float(cpt.iloc[0][f'P({var_name}={val})']) for val in var_vals]
+            gum_bn.cpt(var_name).fillWith(prob_vector)
+        else:
+            # Para cada fila de la tabla, insertar los valores
+            for _, row in cpt.iterrows():
+                parent_inst = {p: str(row[p]) for p in parents}
+                for val in var_vals:
+                    full_inst = {**parent_inst, var_name: str(val)}
+                    prob = float(row[f'P({var_name}={val})'])
+                    gum_bn.cpt(var_name)[full_inst] = prob
+    return gum_bn
 
     
